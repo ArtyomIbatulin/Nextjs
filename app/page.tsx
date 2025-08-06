0e91d29576c45352b17090248bd3e3da9601f70c
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-08-06 21:07:35 +0300

    feat: add shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Next JS</div>;
+  return (
+    <div>
+      Next JS
+      <Button>Войти</Button>
+    </div>
+  );
 }

b62b63303a886446675759a8a3cfa058e4d8676e
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2025-08-11 20:52:38 +0300

    chore: change title on the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Bookstore | Главная",
+  title: "Next Js | Главная",
 };
 
 export default function RootLayout({
